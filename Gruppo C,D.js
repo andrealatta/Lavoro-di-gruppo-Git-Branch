e4cf3e35c6e0720be8cff827a61855b6bce6d52a
CimiBatu <samuelelc2006@gmail.com>
2025-01-07 21:58:00 +0100

    Update Gruppo C,D.js
    inizio codice

--- Gruppo C,D.js
+++ Gruppo C,D.js
@@ -1,1 +1,7 @@
+function generatePassword() {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
+    let password = '';
+    const lengthInput = document.getElementById('passwordLength');
+    const length = parseInt(lengthInput.value) || 12; // Default length is 12
+    for (let i = 0; i < length; i++) {
 
