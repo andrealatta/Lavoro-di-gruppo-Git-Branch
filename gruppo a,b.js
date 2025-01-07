04cf55d88edcd223b816ad072c4d889a2d2097a6
andrealatta <andrealattaarturo@gmail.com>
2025-01-07 20:59:18 +0100

    Update gruppo a,b.js

--- gruppo a,b.js
+++ gruppo a,b.js
@@ -1,1 +1,12 @@
+function generatePassword() {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
+    let password = '';
+    const length = 12; // Lunghezza predefinita della password
+    for (let i = 0; i < length; i++) {
+        password += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    document.getElementById('passwordOutput').textContent = password;
+}
+
+document.getElementById('generatePassword').addEventListener('click', generatePassword);
 
