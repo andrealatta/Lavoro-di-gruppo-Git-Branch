b059e4ac14dffe96989c25baba6a31d9b91207db
Andrea765205 <mariettagoccioandrea@gmail.com>
2025-01-08 09:50:05 +0100

    Update Gruppo C,D.js

--- Gruppo C,D.js
+++ Gruppo C,D.js
@@ -4,4 +4,9 @@ function generatePassword() {
     const lengthInput = document.getElementById('passwordLength');
     const length = parseInt(lengthInput.value) || 12; // Default length is 12
     for (let i = 0; i < length; i++) {
+        password += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    document.getElementById('passwordOutput').textContent = password;
+}
 
+document.getElementById('generatePassword').addEventListener('click', generatePassword);
